ed66557f50a87fcd6046c636bfac380771f310c8
Jorge Clavijo <jym272@gmail.com>
2023-04-22 02:12:59 -0300

    feat: with concurrency 1

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,7 @@ import { createStripeClient } from '@stripe/stripe';
 const { server } = initializeSetup();
 
 const PORT = getEnvOrFail('PORT');
-//
+// This is a comment
 void (async () => {
   const queueGroupName = 'payments-service';
   try {
